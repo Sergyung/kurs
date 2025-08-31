2df33493cea406c9b3faecf07298ed534b145847
Sergej A <sergbest@mail.ru>
2025-08-31 22:23:05 +0300

    edit styles

--- sourse/router.js
+++ sourse/router.js
@@ -1,10 +1,11 @@
-import { Router, urlencoded } from 'express';
+import { Router, urlencoded, static as staticMiddleware } from 'express';
 import methodOverride from 'method-override';
 
 import { mainPage, detailPage, setDone } from './controllers/valute.js';
 
 const router = Router();
 
+router.use(staticMiddleware('public'));
 router.use(urlencoded({ extended: true }));
 router.use(methodOverride('_method'));
 
